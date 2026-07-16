6ede5b412ce1f6ae693b659fb97a9ad8d25dbd01
Krivodanov <ccha7424@gmail.com>
2026-07-16 14:32:00 +0500

    theme3 OOP practic_1/2

--- test_code.py
+++
@@ -1,5 +0,0 @@
-from decimal import Decimal
-
-number = 123.19484
-round_number = Decimal(str(number)).quantize(Decimal('0.01'))
-print(round_number)
--- theme_practic.py
+++ theme_practic.py
@@ -1,24 +1,40 @@
-from decimal import Decimal
+# Импортируйте необходимые модули.
+from datetime import datetime 
+# Укажите два параметра функции:
+def validate_record(name, date_str):
+    # Напишите код, верните булево значение.
+    try:
+        date_obj = datetime.strptime(date_str, '%d.%m.%Y').date()
+    except ValueError:
+        print(f'Некорректный формат даты в записи: {name}, {date_str}')
+        return False 
+    else:
+        return True
 
-class Customer:
-    def __init__(self, name, discount=10):
-        self.name = name
-
-    __discount = 10
-        # Добавьте сюда атрибут "скидка" со значением по умолчанию 10.
-    def get_price(self, price):
-        return round((price - ( (price / 100) * self.__discount) ), 2)
-
-    # Реализуйте методы get_price() и set_discount().
-
-    def set_discount(self, new_discount):
-        if (new_discount <= 80) and (new_discount > 0):
-            self.__discount = new_discount
-        if new_discount > 80:
-            self.__discount = 80
+# Укажите параметры функции:
+def process_people(data):
+    # Объявите счётчики.
+    good_count = 0
+    bad_count = 0
+    # в каждой паре значений из списка data
+    # проверьте корректность формата даты рождения
+    # и в зависимости от результата проверки увеличьте один из счётчиков.    
+    for inf in data:
+        if validate_record(*inf):
+            good_count += 1
+        else:
+            bad_count += 1
+    return {'good': good_count, 'bad': bad_count}
 
 
-customer = Customer("Иван Иванович")
-print(customer.get_price(100))
-customer.set_discount(19)
-print(customer.get_price(1114))
+data = [
+    ('Акакий Башмачкинъ',    '23 марта 1791 года'),
+    ('Яков Степанов', 'Двадцать шестое июля 1971'),
+    ('Потап Алексеев', '16.09.1990'),
+    ('Евгений Женин', '5 декабря 1984'),
+    ('Кондрат Александров', '18.01.1994')
+] 
+statistics = process_people(data)
+
+print(f"Корректных записей: {statistics['good']}")
+print(f"Некорректных записей: {statistics['bad']}")
