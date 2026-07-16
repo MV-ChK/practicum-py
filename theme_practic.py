ed51f1e18b6dac600f1c2a9e370316473d08dba1
Krivodanov <ccha7424@gmail.com>
2026-07-16 15:02:57 +0500

    theme3 OOP practic_2/2

--- theme_practic.py
+++ theme_practic.py
@@ -1,40 +1,27 @@
-# Импортируйте необходимые модули.
-from datetime import datetime 
-# Укажите два параметра функции:
-def validate_record(name, date_str):
-    # Напишите код, верните булево значение.
-    try:
-        date_obj = datetime.strptime(date_str, '%d.%m.%Y').date()
-    except ValueError:
-        print(f'Некорректный формат даты в записи: {name}, {date_str}')
-        return False 
-    else:
-        return True
-
-# Укажите параметры функции:
-def process_people(data):
-    # Объявите счётчики.
-    good_count = 0
-    bad_count = 0
-    # в каждой паре значений из списка data
-    # проверьте корректность формата даты рождения
-    # и в зависимости от результата проверки увеличьте один из счётчиков.    
-    for inf in data:
-        if validate_record(*inf):
-            good_count += 1
-        else:
-            bad_count += 1
-    return {'good': good_count, 'bad': bad_count}
 
 
-data = [
-    ('Акакий Башмачкинъ',    '23 марта 1791 года'),
-    ('Яков Степанов', 'Двадцать шестое июля 1971'),
-    ('Потап Алексеев', '16.09.1990'),
-    ('Евгений Женин', '5 декабря 1984'),
-    ('Кондрат Александров', '18.01.1994')
-] 
-statistics = process_people(data)
+from datetime import datetime
+from random import sample
 
-print(f"Корректных записей: {statistics['good']}")
-print(f"Некорректных записей: {statistics['bad']}")
+
+def choose_days():
+    # Определяем диапазон дат первой половины месяца.
+    first_month_half = list(range(1, 16))
+
+    # Выбор трёх случайных чисел:
+    random_days = sample(first_month_half, k=3)
+    # Cортировка этих чисел по возрастанию:
+    sorted_days = sorted(random_days)
+
+    # Получаем сегодняшнюю дату.
+    # На её основе будут генерироваться даты для занятий:
+    now = datetime.now()
+
+    # Чтобы было проще формировать сообщение, начнём цикл с 1.
+    for i in range(0, 3):
+        # Генерируем дату занятия, подменяя номер дня в сегодняшней дате.
+        practice_day = now.replace(day=sorted_days[i]).strftime("%d.%m.%Y")
+        print(f'{i+1}-е занятие: {practice_day}')
+
+
+choose_days()
