51c81f791d299e58fbe2ac1dae67edb7300c125c
Krivodanov <ccha7424@gmail.com>
2026-07-26 21:48:36 +0500

    4 theme practic 2/3

--- theme_practic.py
+++ theme_practic.py
@@ -1,34 +1,30 @@
-# Числа Фибоначчи — это последовательность чисел, где первые два числа — это 0 и 1,
-# а каждое последующее число вычисляется как сумма двух предыдущих. 
-# Например, вот первые шесть чисел последовательности Фибоначчи:
-# 0, 1, 1, 2, 3, 5 
-# Напишите функцию-генератор fibonacci(n), которая вычисляет элементы последовательности Фибоначчи до n-ого числа.
-
-
-
-def fibonacci(n):
-    
-    # a = 0
-    # b = 1
-    # for i in range(n):
-    #     yield a
-    #     current = a
-    #     a = b
-    #     b = current + b
-
-    fib_list = [0, 1]
-    c = 0
-    while c < n:
+# Напишите декоратор obfuscator
+def obfuscator(func):
+    def wrapper():
+        dct = func()
         
-        if len(fib_list) == 2:
-            yield 0; c += 1
-            yield 1; c += 1
-            
-        temp_x = fib_list[-1] + fib_list[-2]
-        fib_list.append(temp_x)
-        yield temp_x; c += 1
+        name = dct['name']
+        password = dct['password']
+        
+        dct['name'] = (
+            name[0] + 
+            ('*' * (len(name) - 2) ) + 
+            name[-1])
+        
+        dct['password'] ='*' * len(password)
+        
+        return dct
+    return wrapper
 
 
-sequence = fibonacci(12)
-for number in sequence:
-    print(number)
+
+
+@obfuscator
+def get_credentials():
+    return {
+        'name': 'StasBasov',
+        'password': 'iamthebest'
+    }
+
+
+print(get_credentials())
